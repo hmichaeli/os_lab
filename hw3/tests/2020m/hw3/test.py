7b2d3ddff6c339c92f6aa0b41576aa6e862c8611
hagaymi <hagaymichaeli@gmail.com>
2020-09-12 11:24:40 +0300

    [hw3] add tests

--- hw3/tests/2020m/hw3/test.py
+++ hw3/tests/2020m/hw3/test.py
@@ -58,15 +58,59 @@ def test2():
     else:
         time.sleep(3)
         date_time = time.localtime()
+        os.system('dmesg')
+        time.sleep(1)
+        print("\n\n\n")
         print("[parent] set child policy date and time:",date_time)
         pyPolicy.set_policy(cpid, 1, 5)
+        
+        for i in range(4):
+            os.system('dmesg')
+            print("\n\n\n")
+            time.sleep(2)
+        
+        print("[parent] wait")
+        os.wait()
+        print("[parent] done")
+    	# print("[parent] wait:",date_time)
+        # time.sleep(30)
+    	# print("[parent] kill child:",date_time)
+        # os.kill(cpid, 9)
+    	# print("[parent] done:",date_time)
+
+
+def test3():
+    pid = os.getpid()
+    log("[parent] pid: ", pid)
+    cpid = os.fork()
+    if (cpid == 0):
+        pid = os.getpid()
+        log("[child] pid: ", pid)
+    # In child, spin for 2-3 seconds before exiting
+        for i in range(20):
+            log("[child]")
+            for k in xrange(12000000):
+                pass
+
+        os._exit(0)
+    else:
+        time.sleep(3)
+        log("[parent] set child policy date and time:")
+        pyPolicy.set_policy(cpid, 2, 5)
+        os.system('dmesg')
+        time.sleep(2)
+        os.system('dmesg')
+        time.sleep(2)
+        os.system('dmesg')
+        time.sleep(2)
     	# print("[parent] wait:",date_time)
         time.sleep(20)
-    	print("[parent] kill child:",date_time)
-        kill(cpid, 9)
-    	print("[parent] done:",date_time)
+    	log("[parent] kill child:")
+        os.kill(cpid, 9)
+    	log("[parent] done:")
 
 if __name__ == "__main__":
+    os.system('dmesg -c > /dev/null')
 #    test1()
     test2()
    # test_sleep()
